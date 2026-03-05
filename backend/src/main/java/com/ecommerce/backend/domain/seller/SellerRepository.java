271fba8466dd07bb408f44ab6f6d9ffe00356869
AlbertNewton254 <miguelmochizukisilva@gmail.com>
2026-03-05 18:12:55 -0300

    feat: add Seller repository

---
+++ backend/src/main/java/com/ecommerce/backend/domain/seller/SellerRepository.java
@@ -0,0 +1,15 @@
+package com.ecommerce.backend.domain.seller;
+
+import com.ecommerce.backend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SellerRepository extends JpaRepository<SellerProfile, Long> {
+
+    Optional<SellerProfile> findByUser(User user);
+
+    boolean existsByUser(User user);
+}
